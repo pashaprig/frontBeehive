212173789693c1e0b81ba4de36a9089d2fa444a4
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2024-07-24 18:10:00 +0300

    Add translation

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,11 +1,20 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { AppRoute } from '../../consts';
+import { useTranslation } from 'react-i18next';
+import LangChange from '../lang-change/lang-change';
 
 function Logo(): JSX.Element {
+  const { t } = useTranslation();
   return (
-    <Link className="header__logo-link header__logo-link--active" to={AppRoute.Main}>
-      <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="{81}" height="{41}" />
-    </Link>
+    <>
+      <div>
+        <LangChange />
+        <span>{t('welcome')}</span>
+      </div>
+      <Link className="header__logo-link header__logo-link--active" to={AppRoute.Main}>
+        <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="{81}" height="{41}" />
+      </Link>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { store } from './store';
+import './i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
